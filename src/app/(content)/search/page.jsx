8c852b625870069e74fbc112a2beac12c7cf3c35
kyle41io <vankien41hust@gmail.com>
2023-10-25 15:18:41 +0700

    feat: add pages

--- src/app/(content)/search/page.jsx
+++ src/app/(content)/search/page.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 const page = () => {
-  return <div>playlist page</div>;
+  return <div>search page</div>;
 };
 
 export default page;
--- src/app/(content)/stream/page.jsx
+++ src/app/(content)/stream/page.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 const page = () => {
-  return <div>playlist page</div>;
+  return <div>stream page</div>;
 };
 
 export default page;
