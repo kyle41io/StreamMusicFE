a08acd191ee551f531b8bea69afacbcbdd77c4e6
HoangDilo <truonghoang14122003@gmail.com>
2023-10-30 15:54:35 +0700

    Revert "Merge branch 'main' into develop"
    This reverts commit e537ec10ba81183ed09e7fe1f1c8ade8a00814a2, reversing
    changes made to 7db1372983b6b022642d574eecd66fbdf2e84bff.

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,27 +1,18 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
     extend: {
-      colors: {
-        primary: "#FF6B00",
-        primaryBlack: "#2B2B2B",
-        primaryLink: "#0094FF",
-        primarySuccess: "#1AB232",
-        primaryError: "#FF4040",
-        primaryGray: "#979797",
+      backgroundImage: {
+        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
+        'gradient-conic':
+          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
     },
   },
   plugins: [],
-  safelist: [
-    {
-      pattern:
-        /(bg|text|border)-(primary|primaryBlack|primaryLink|primaryGray|primarySuccess|primaryError)/,
-    },
-  ],
-};
+}
