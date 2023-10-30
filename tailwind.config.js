5f5e31022e8f9091feb4d6085a63c082a443c283
Datt1409 <112248757+Datt1409@users.noreply.github.com>
2023-10-30 15:19:03 +0700

    config color

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,27 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      colors: {
+        primary: "#FF6B00",
+        primaryBlack: "#2B2B2B",
+        link: "#0094FF",
+        success: "#1AB232",
+        _error: "#FF4040",
+        primaryGray: "#979797",
       },
     },
   },
   plugins: [],
-}
+  safelist: [
+    {
+      pattern:
+        /(bg|text|border)-(primary|primaryBlack|link|primaryGray|success|_error)/,
+    },
+  ],
+};
