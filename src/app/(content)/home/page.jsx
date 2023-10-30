e481c71c5f598e36637eefff758271671f23b008
kyle41io <vankien41hust@gmail.com>
2023-10-30 13:13:44 +0700

    refactor code

--- src/app/(content)/home/page.jsx
+++ src/app/(content)/home/page.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 
-const DetailMusic = () => {
-  return <div>Detail Music</div>;
+const Home = () => {
+  return <div>Home</div>;
 };
 
-export default DetailMusic;
+export default Home;
