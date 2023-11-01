cc1a0b2f8c059adf7eb61a27c574b7da7ecc7197
HoangDilo <truonghoang14122003@gmail.com>
2023-11-01 14:53:30 +0700

    completed UI real

--- src/components/auth/sign-up/UploadImg.jsx
+++ src/components/auth/sign-up/UploadImg.jsx
@@ -1,7 +1,14 @@
 'use client'
+import { useState } from 'react'
 import styles from './../../../styles/auth/sign-up/UploadImg.module.css'
 
 function UploadImg() {
+    const [fileName, setFileName] = useState('No file chosen');
+
+    const handleChange = () => {
+        setFileName(event.target.files[0].name);
+    }
+
     return (
         <div className={styles['upload-avt']}>
             <div className={styles['upload-button-container']}>
@@ -11,11 +18,11 @@ function UploadImg() {
                     <span className='link text-xs'>
                         Upload Avatar
                     </span>
-                    <input type="file" name="" id="input-avt" hidden/>
+                    <input type="file" name="" id="input-avt" hidden onChange={handleChange}/>
                 </label>
             </div>
-            <span>
-                No file chosen
+            <span className={styles['file-name-label']}>
+                {fileName}
             </span>
         </div>
     )
