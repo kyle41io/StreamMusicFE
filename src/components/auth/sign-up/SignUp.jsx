90207a77db3a7b2df81deb7110664bf49335fae6
HoangDilo <truonghoang14122003@gmail.com>
2023-11-01 09:43:28 +0700

    check out

---
+++ src/components/auth/sign-up/SignUp.jsx
@@ -0,0 +1,16 @@
+import styles from './../../../styles/auth/sign-up/SignUp.module.css'
+
+function SignUp() {
+    return (
+        <div className={styles['main-container']}>
+            <div className={styles['signup-container']}>
+                <div className=''>
+
+                </div>
+            </div>
+        </div>
+
+    )
+}
+
+export default SignUp
