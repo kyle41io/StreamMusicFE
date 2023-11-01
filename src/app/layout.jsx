b21fc5ca786eab9b2f94ed5bc02a95c1d48f6c06
HoangDilo <truonghoang14122003@gmail.com>
2023-11-01 09:45:58 +0700

    Revert "Merge pull request #6 from kyle41io/features/Header"
    This reverts commit 11e6572437195a8bf595d0661d702d96d32808b1, reversing
    changes made to a08acd191ee551f531b8bea69afacbcbdd77c4e6.

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 import { Poppins } from "next/font/google";
 import "./globals.css";
 import Header from "@/components/shared/Header";
@@ -6,6 +7,12 @@ const poppins = Poppins({
   subsets: ["latin"],
   weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
 });
+=======
+import { Inter } from "next/font/google";
+import "../styles/globals.css";
+
+const inter = Inter({ subsets: ["latin"] });
+>>>>>>> parent of 11e6572 (Merge pull request #6 from kyle41io/features/Header)
 
 export const metadata = {
   title: "Dolphin Music Stream",
@@ -15,12 +22,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={poppins.className}>
-        <div className="flex flex-col">
-          <Header />
-          {children}
-        </div>
-      </body>
+      <body className={inter.className}>{children}</body>
     </html>
   );
 }
