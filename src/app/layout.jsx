27f9c3ec8fa02ff5ef069e92ede4b9056d04752e
HoangDilo <truonghoang14122003@gmail.com>
2023-11-01 10:08:39 +0700

    fix conflict

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,25 +1,13 @@
-<<<<<<< HEAD
-<<<<<<< HEAD
+
 import { Poppins } from "next/font/google";
-=======
-import { Inter } from "next/font/google";
->>>>>>> parent of 0cee53e (header ui)
+
 import "./globals.css";
 
-<<<<<<< HEAD
 const poppins = Poppins({
   subsets: ["latin"],
   weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
 });
-=======
-import { Inter } from "next/font/google";
-import "../styles/globals.css";
 
-const inter = Inter({ subsets: ["latin"] });
->>>>>>> parent of 11e6572 (Merge pull request #6 from kyle41io/features/Header)
-=======
-const inter = Inter({ subsets: ["latin"] });
->>>>>>> parent of 0cee53e (header ui)
 
 export const metadata = {
   title: "Dolphin Music Stream",
