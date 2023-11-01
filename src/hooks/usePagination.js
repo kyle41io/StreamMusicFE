6f7437015774177680d92811f66e7102d3c79c6b
Datt1409 <112248757+Datt1409@users.noreply.github.com>
2023-11-01 14:32:50 +0700

    insert link to the home page

---
+++ src/hooks/usePagination.js
@@ -0,0 +1,13 @@
+import React, { useMemo } from "react";
+
+export default function usePagination({
+  totalCount,
+  pageSize,
+  siblingCount = 1,
+  currentPage,
+}) {
+  const paginationRange = useMemo(() => {
+    
+  }, []);
+  return paginationRange;
+}
