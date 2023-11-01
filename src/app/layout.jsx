d5d6f7dc9a52e90e6f7672551b23228502d63470
HoangDilo <truonghoang14122003@gmail.com>
2023-11-01 09:47:02 +0700

    Revert "Merge pull request #6 from kyle41io/features/Header"
    This reverts commit 11e6572437195a8bf595d0661d702d96d32808b1, reversing
    changes made to a08acd191ee551f531b8bea69afacbcbdd77c4e6.

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,8 +1,12 @@
 <<<<<<< HEAD
+<<<<<<< HEAD
 import { Poppins } from "next/font/google";
+=======
+import { Inter } from "next/font/google";
+>>>>>>> parent of 0cee53e (header ui)
 import "./globals.css";
-import Header from "@/components/shared/Header";
 
+<<<<<<< HEAD
 const poppins = Poppins({
   subsets: ["latin"],
   weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
@@ -13,6 +17,9 @@ import "../styles/globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
 >>>>>>> parent of 11e6572 (Merge pull request #6 from kyle41io/features/Header)
+=======
+const inter = Inter({ subsets: ["latin"] });
+>>>>>>> parent of 0cee53e (header ui)
 
 export const metadata = {
   title: "Dolphin Music Stream",
