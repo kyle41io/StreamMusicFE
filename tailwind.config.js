0f66aa54068e521d8d9b947c5f4cb27eb4c60a5d
HoangDilo <truonghoang14122003@gmail.com>
2023-11-01 10:45:22 +0700

    fix conflict tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
-<<<<<<< HEAD
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
-=======
       colors: {
         primary: "#FF6B00",
         primaryBlack: "#2B2B2B",
@@ -20,19 +14,16 @@ module.exports = {
         primarySuccess: "#1AB232",
         primaryError: "#FF4040",
         primaryGray: "#979797",
->>>>>>> parent of 0cee53e (header ui)
+        secondaryBlack: "#24292F",
+        thirdBlack: "#0f0f0f",
       },
     },
   },
   plugins: [],
-<<<<<<< HEAD
-}
-=======
   safelist: [
     {
       pattern:
-        /(bg|text|border)-(primary|primaryBlack|primaryLink|primaryGray|primarySuccess|primaryError)/,
+        /(bg|text|border)-(primary|primaryBlack|primaryLink|primaryGray|primarySuccess|primaryError|secondaryBlack|thirdBlack)/,
     },
   ],
 };
->>>>>>> parent of 0cee53e (header ui)
