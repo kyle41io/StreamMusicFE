0e63253a9c4588c1efced2ce1559a3e0cb2a11d2
HoangDilo <truonghoang14122003@gmail.com>
2023-11-01 09:55:48 +0700

    check out

--- src/components/auth/sign-up/SignUp.jsx
+++ src/components/auth/sign-up/SignUp.jsx
@@ -2,10 +2,9 @@ import styles from './../../../styles/auth/sign-up/SignUp.module.css'
 
 function SignUp() {
     return (
-        <div className={styles['main-container']}>
+        <div className={styles['main-session']}>
             <div className={styles['signup-container']}>
                 <div className=''>
-
                 </div>
             </div>
         </div>
