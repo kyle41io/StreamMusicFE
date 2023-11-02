317d06c43b054e5225870073a019b7858dee11d9
HoangDilo <truonghoang14122003@gmail.com>
2023-11-02 14:18:19 +0700

    home-slider: complete UI

---
+++ src/constant/topPlaylist.js
@@ -0,0 +1,40 @@
+const TOP_PLAYLIST = [
+    {
+        member: 'N D T',
+        title: 'Vao nghe cho vui de',
+        img_src: 'https://res-console.cloudinary.com/dbda40mpf/thumbnails/v1/image/upload/v1698906959/Y2xkLXNhbXBsZS01/grid_landscape',
+        views: 1301,
+    },
+    {
+        member: 'ABC',
+        title: 'aduududuasud',
+        img_src: 'https://res-console.cloudinary.com/dbda40mpf/thumbnails/v1/image/upload/v1698906959/Y2xkLXNhbXBsZS01/grid_landscape',
+        views: 1102,
+    },
+    {
+        member: 'Em hoeng',
+        title: 'aloooooooo',
+        img_src: 'https://res-console.cloudinary.com/dbda40mpf/thumbnails/v1/image/upload/v1698906959/Y2xkLXNhbXBsZS01/grid_landscape',
+        views: 977,
+    },
+    {
+        member: 'NNN',
+        title: 'NHu nhuhjajhflk',
+        img_src: 'https://res-console.cloudinary.com/dbda40mpf/thumbnails/v1/image/upload/v1698906959/Y2xkLXNhbXBsZS01/grid_landscape',
+        views: 864,
+    },
+    {
+        member: 'OOOO',
+        title: 'MMMMMMMMMMMMMMMMMMMMMMMMMM',
+        img_src: 'https://res-console.cloudinary.com/dbda40mpf/thumbnails/v1/image/upload/v1698906959/Y2xkLXNhbXBsZS01/grid_landscape',
+        views: 744,
+    },
+    {
+        member: 'Anh bay cho',
+        title: 'SIuuuuuuuuuuuu',
+        img_src: 'https://res-console.cloudinary.com/dbda40mpf/thumbnails/v1/image/upload/v1698906959/Y2xkLXNhbXBsZS01/grid_landscape',
+        views: 234,
+    },
+]
+
+export default TOP_PLAYLIST
