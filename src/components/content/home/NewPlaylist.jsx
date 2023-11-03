1714bbaa6788a8abd4e20be8a8bc6ab5d41f32c4
HoangDilo <truonghoang14122003@gmail.com>
2023-11-03 17:32:14 +0700

    fix some UI issues

--- src/components/content/home/NewPlaylist.jsx
+++ src/components/content/home/NewPlaylist.jsx
@@ -1,16 +1,29 @@
-'use client'
+"use client";
 
-import PagingBar from "@/components/shared/PagingBar"
-import { useState } from "react"
+import styles from "./../../../styles/content/home/NewPlaylist.module.css";
+import PagingBar from "@/components/shared/PagingBar";
+import { useTranslations } from "next-intl";
+import { useState } from "react";
 
 function NewPlaylist() {
-    const [currentPage, setCurrentPage] = useState(1);
+  const t = useTranslations("Home");
+  const [currentPage, setCurrentPage] = useState(1);
 
-    return (
-        <div>
-            <PagingBar currentPage={currentPage} onClick={setCurrentPage} maxPage={12}/>
+  return (
+    <div className={styles["main-container"]}>
+      <p className={styles.title}>{t("new_playlist")}</p>
+      <div className={styles['main-content']}>
+        <div className={styles['header']}>
+
         </div>
-    )
+        <PagingBar
+          currentPage={currentPage}
+          onClick={setCurrentPage}
+          maxPage={12}
+        />
+      </div>
+    </div>
+  );
 }
 
-export default NewPlaylist
+export default NewPlaylist;
