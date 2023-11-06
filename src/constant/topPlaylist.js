68c60358e9ffd66e45913bd78769b690d3ab24cb
HoangDilo <129303901+HoangDilo@users.noreply.github.com>
2023-11-06 16:12:16 +0700

    New-Playlist: done UI
    need API for testing pagination

--- src/constant/topPlaylist.js
+++ src/constant/topPlaylist.js
@@ -2,37 +2,37 @@ const TOP_PLAYLIST = [
     {
         member: 'N D T',
         title: 'Vao nghe cho vui de',
-        img_src: 'https://res-console.cloudinary.com/dbda40mpf/thumbnails/v1/image/upload/v1698906959/Y2xkLXNhbXBsZS01/grid_landscape',
+        img_src: 'https://res.cloudinary.com/dbda40mpf/image/upload/v1698906959/cld-sample-5.jpg',
         views: 1301,
     },
     {
         member: 'ABC',
         title: 'aduududuasud',
-        img_src: 'https://res-console.cloudinary.com/dbda40mpf/thumbnails/v1/image/upload/v1698906959/Y2xkLXNhbXBsZS01/grid_landscape',
+        img_src: 'https://res.cloudinary.com/dbda40mpf/image/upload/v1698906959/cld-sample-5.jpg',
         views: 1102,
     },
     {
         member: 'Em hoeng',
         title: 'aloooooooo',
-        img_src: 'https://res-console.cloudinary.com/dbda40mpf/thumbnails/v1/image/upload/v1698906959/Y2xkLXNhbXBsZS01/grid_landscape',
+        img_src: 'https://res.cloudinary.com/dbda40mpf/image/upload/v1698906959/cld-sample-5.jpg',
         views: 977,
     },
     {
         member: 'NNN',
         title: 'NHu nhuhjajhflk',
-        img_src: 'https://res-console.cloudinary.com/dbda40mpf/thumbnails/v1/image/upload/v1698906959/Y2xkLXNhbXBsZS01/grid_landscape',
+        img_src: 'https://res.cloudinary.com/dbda40mpf/image/upload/v1698906959/cld-sample-5.jpg',
         views: 864,
     },
     {
         member: 'OOOO',
         title: 'MMMMMMMMMMMMMMMMMMMMMMMMMM',
-        img_src: 'https://res-console.cloudinary.com/dbda40mpf/thumbnails/v1/image/upload/v1698906959/Y2xkLXNhbXBsZS01/grid_landscape',
+        img_src: 'https://res.cloudinary.com/dbda40mpf/image/upload/v1698906959/cld-sample-5.jpg',
         views: 744,
     },
     {
         member: 'Anh bay cho',
         title: 'SIuuuuuuuuuuuu',
-        img_src: 'https://res-console.cloudinary.com/dbda40mpf/thumbnails/v1/image/upload/v1698906959/Y2xkLXNhbXBsZS01/grid_landscape',
+        img_src: 'https://res.cloudinary.com/dbda40mpf/image/upload/v1698906959/cld-sample-5.jpg',
         views: 234,
     },
 ]
