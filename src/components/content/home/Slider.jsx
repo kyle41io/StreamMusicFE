0451d364ceeddd774429dcf9571110036edb6cc8
HoangDilo <129303901+HoangDilo@users.noreply.github.com>
2023-11-10 20:08:23 +0700

    optimize slider a little bit

--- src/components/content/home/Slider.jsx
+++ src/components/content/home/Slider.jsx
@@ -5,46 +5,29 @@ import TOP_PLAYLIST from '@/constant/topPlaylist'
 
 function Slider({ topPlayList_t, list }) {
     const [showBlurLeft, setShowBlurLeft] = useState(false);
-    const [showBlurRight, setShowBlurRight] = useState(true);
 
     const sliderRef = useRef();
     const containerRef = useRef();
-    const interValLeft = useRef();
-    const interValRight = useRef();
+    const interValFake = useRef();
 
     useEffect(() => {
         sliderRef.current.style.left = '0px';
         sliderRef.current.style.right = `${(-1790 + containerRef.current.offsetWidth)}px`;
     }, [])
 
-    const handleGoLeft = () => {
-        interValLeft.current = setInterval(() => {
-            if (parseInt(sliderRef.current.style.left) >= 0) {
-                clearInterval(interValLeft);
-            } else {
+    const handleMove = (isLeft) => {
+        clearInterval(interValFake.current)
+        interValFake.current = setInterval(() => {
+            if (isLeft && parseInt(sliderRef.current.style.left) < 0) {
                 sliderRef.current.style.left = `${parseInt(sliderRef.current.style.left) + 10}px`
                 sliderRef.current.style.right = `${parseInt(sliderRef.current.style.right) - 10}px`
-                setShowBlurRight(true);
-                setShowBlurLeft(false);
-            }
-        }, 50);
-    }
-
-    const handleGoRight = () => {
-        interValRight.current = setInterval(() => {
-            if (parseInt(sliderRef.current.style.left) <= (-1790 + containerRef.current.offsetWidth)) {
-                clearInterval(interValRight)
-            } else {
+            } else if (!isLeft && parseInt(sliderRef.current.style.left) > (-1790 + containerRef.current.offsetWidth)) {
                 sliderRef.current.style.left = `${parseInt(sliderRef.current.style.left) - 10}px`
                 sliderRef.current.style.right = `${parseInt(sliderRef.current.style.right) + 10}px`
-                setShowBlurLeft(true);
-                setShowBlurRight(false);
             }
-        }, 50);
-    }
+            setShowBlurLeft(!isLeft)
+        }, 50)
 
-    const handleResize = () => {
-        console.log('resized!');
     }
 
     const formatViews = (views) => {
@@ -56,20 +39,20 @@ function Slider({ topPlayList_t, list }) {
     }
 
     return (
-        <div className={styles['slider-container']} onResize={handleResize}>
+        <div className={styles['slider-container']}>
             <div className={styles['slider-header']}>
                 <p className={styles['slider-title']}>
                     {topPlayList_t}
                 </p>
                 <div className={styles['move-buttons']}>
-                    <button className={`${styles['move-button']} left-arrow`} onMouseDown={handleGoLeft} onMouseUp={() => clearInterval(interValLeft.current)}></button>
-                    <button className={`${styles['move-button']} right-arrow`} onMouseDown={handleGoRight} onMouseUp={() => clearInterval(interValRight.current)}></button>
+                    <button className={`${styles['move-button']} left-arrow`} onMouseDown={() => handleMove(true)} onMouseUp={() => clearInterval(interValFake.current)}></button>
+                    <button className={`${styles['move-button']} right-arrow`} onMouseDown={() => handleMove(false)} onMouseUp={() => clearInterval(interValFake.current)}></button>
                 </div>
             </div>
             <div className={styles['main-slider']} ref={containerRef}>
                 {showBlurLeft && <div className={styles['blur-left']}>
                 </div>}
-                {showBlurRight && <div className={styles['blur-right']}>
+                {!showBlurLeft && <div className={styles['blur-right']}>
                 </div>}
                 <div className={styles['slider']} ref={sliderRef}>
                     {TOP_PLAYLIST.map((item, index) => <div className={styles['card']} key={index}>
