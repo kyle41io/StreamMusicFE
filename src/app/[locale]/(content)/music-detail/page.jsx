4f890186af4820db247518a7a07a45832deb946b
Datt1409 <112248757+Datt1409@users.noreply.github.com>
2023-11-14 21:45:14 +0700

    feat: create current playlsit

--- src/app/[locale]/(content)/music-detail/page.jsx
+++ src/app/[locale]/(content)/music-detail/page.jsx
@@ -3,8 +3,21 @@ import AlbumItem from "@/components/music-detail/AlbumItem";
 import Information from "@/components/music-detail/Information";
 import ListAction from "@/components/ListAction";
 import MusicPlayer from "@/components/music-detail/MusicPlayer";
+import { tracks } from "@/constant/songs(test)";
+import { useContext, useState } from "react";
+import { DetailProvider } from "@/store/MusicDetailProvider";
 
 const DetailMusic = () => {
+  const [isPlaying, setIsPlaying] = useState(false);
+  const [selectedTrack, setSelectedTrack] = useState(null);
+  const { currentIndex, setCurrentIndex } = useContext(DetailProvider);
+
+  const handlePlayTrack = (track) => {
+    setIsPlaying(true);
+    setSelectedTrack(track);
+    setCurrentIndex(track.id);
+  };
+
   return (
     <div className="flex mx-auto gap-[4%]">
       <div className="w-[64%] flex flex-col gap-6">
--- src/components/music-detail/AlbumItem.jsx
+++ src/components/music-detail/AlbumItem.jsx
@@ -1,19 +1,24 @@
 "use client";
-import { useState } from "react";
-import thumbnail from "@/assets/images/album_thumb.png";
 import { IoStatsChartSharp } from "react-icons/io5";
 
-export default function AlbumItem() {
-  const [isPlaying, setIsPlaying] = useState(true);
+export default function AlbumItem({ track, setIsPlaying, isPlaying }) {
+  const handlePlayClick = () => {
+    setIsPlaying(); // Make sure to pass track information if needed
+  };
 
   return (
     <>
-      <div className="w-full h-20 flex py-2 px-3 gap-3 items-center hover:bg-secondaryGray cursor-pointer">
-        {isPlaying ? (
-          <IoStatsChartSharp className="text-primary text-sm" />
-        ) : (
-          <p className="text-sm">1</p>
-        )}
+      <div
+        className="w-full h-20 flex py-2 px-3 gap-4 items-center hover:bg-secondaryGray cursor-pointer"
+        onClick={handlePlayClick}
+      >
+        <div className="w-[12px]">
+          {isPlaying ? (
+            <IoStatsChartSharp size={12} className="text-primary text-sm" />
+          ) : (
+            <p className="text-sm">{track.id + 1}</p>
+          )}
+        </div>
         <div
           className="w-[13%] h-[60px] bg-center object-cover"
           style={{ background: `url(${thumbnail.src})` }}
