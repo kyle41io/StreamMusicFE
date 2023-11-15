3f5be3e97373e4e93866195c046ca948b3a17558
HoangDilo <129303901+HoangDilo@users.noreply.github.com>
2023-11-15 16:15:17 +0700

    edit some clip-path to clipPath

--- src/assets/icons/IcPlayWhite2.jsx
+++ src/assets/icons/IcPlayWhite2.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 export default function IcPlayWhite2() {
     return (
         <svg width="16" height="17" viewBox="0 0 16 17" fill="none" xmlns="http://www.w3.org/2000/svg">
-            <g clip-path="url(#clip0_79_1898)">
+            <g clipPath="url(#clip0_79_1898)">
                 <path d="M5.3335 3.83331V13.1666L12.6668 8.49998L5.3335 3.83331Z" fill="white" />
             </g>
             <defs>
